e171ed6d05d6e4ec194c43d8a3d9182af0707889
IQSitesss <dropsonsss@gmail.com>
2026-02-23 22:04:13 +0200

    add telegram miniapp support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 
+window.Telegram?.WebApp?.ready();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
